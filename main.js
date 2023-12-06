a36034c6e589f51ac0297652676614597da99892
Waleed Lugod <lugodwaleed@gmail.com>
2023-12-07 00:11:55 +0800

    Initial prototype

---
+++ main.js
@@ -0,0 +1,74 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+window.onload = () => {
+
+	init()
+
+}
+
+function init() {
+
+	// get code
+	const codeURL = "https://raw.githubusercontent.com/waleedlugod/jco/main/seamcarver.py"
+	fetch(codeURL)
+	.then(res => res.text())
+	.then(text => createText(text));
+
+}
+
+function createText(text) {
+
+	const codeWrapper = document.createElement("div");
+	codeWrapper.id = "codeWrapper";
+
+	for (var l = 0; l <= text.length; l++) {
+
+		const char = document.createElement("span");
+
+		char.id = "char" + l;
+		char.className = "char";
+		if (/\n/.test(text.charAt(l))) {
+			char.innerHTML = "<br/>"
+		} else if (/\t/.test(text.charAt(l))) {
+			char.innerHTML = "&emsp;"
+		} else if (/ /.test(text.charAt(l))) {
+			char.innerHTML = "&nbsp;"
+		} else {
+			char.innerHTML = text.charAt(l)
+		}
+
+		char.addEventListener("click", () => {
+			burn(char.id)
+		})
+
+		codeWrapper.appendChild(char);
+	}
+
+	document.body.appendChild(codeWrapper);
+
+}
+
+async function burn(id) {
+
+	const char = document.getElementById(id);
+
+	if (char.innerHTML !== "🔥") {
+
+		char.innerHTML = "🔥";
+		console.log(id + " on fire 🔥")
+
+		await delay(200);
+		burn(char.nextSibling.id)
+		burn(char.previousSibling.id)
+
+		await delay(1000);
+		char.remove()
+		console.log(id + " burnt ⚱️")
+
+	}
+
+}
+
+function getRndInt(min,max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
